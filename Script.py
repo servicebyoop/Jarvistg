f6e2d105b3c31fb2b828f4e19d411588c57701ef
servicebyoop <102507731+servicebyoop@users.noreply.github.com>
2022-06-01 06:58:00 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -18,10 +18,10 @@ class script(object):
 - <a href=https://t.me/opgohil>Team opgohil</a>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
-- Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
+- Filter is the feature were users can set automated replies for a particular keyword and jarvistg will respond whenever a keyword is found the message
 
 <b>NOTE:</b>
-1. opgohil should have admin privillage.
+1. jarvistg should have admin privillage.
 2. only admins can add filters in a chat.
 3. alert buttons have a limit of 64 characters.
 
@@ -32,7 +32,7 @@ class script(object):
 • /delall - <code>delete the whole filters in a chat (chat owner only)</code>"""
     BUTTON_TXT = """Help: <b>Buttons</b>
 
-- Eva Maria Supports both url and alert inline buttons.
+- jarvistg Supports both url and alert inline buttons.
 
 <b>NOTE:</b>
 1. Telegram will not allows you to send buttons without any content, so content is mandatory.
@@ -40,7 +40,7 @@ class script(object):
 3. Buttons should be properly parsed as markdown format
 
 <b>URL buttons:</b>
-<code>[Button Text](buttonurl:https://t.me/EvaMariaBot)</code>
+<code>[Button Text](buttonurl:https://t.me/opgohil)</code>
 
 <b>Alert buttons:</b>
 <code>[Button Text](buttonalert:This is an alert message)</code>"""
