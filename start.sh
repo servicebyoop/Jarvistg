dce99a3cc942f874fa00948b2bd3af9813638cb2
servicebyoop <102507731+servicebyoop@users.noreply.github.com>
2022-06-01 07:53:00 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/servicebyoop/Jarvistg /EvaMaria
+  git clone https://github.com/servicebyoop/Jarvistg
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Jarvistg
